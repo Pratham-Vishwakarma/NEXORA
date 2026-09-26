ee6e24cd0680ae567d4bd4e52fa42b9ea2b41280
Pratham Vishwakarma <pratham.vishwakarma125940@gmail.com>
2026-09-27 01:33:01 +0530

    Used logger for BT

--- Data_Feed/src/tel_log_bt.py
+++ Data_Feed/src/tel_log_bt.py
@@ -13,12 +13,12 @@ from serial import SerialException
 # ============================================================
 #                EXPERIMENT VARIABLES
 # ============================================================
-DISTANCE_M = 10.0
+DISTANCE_M = 1.0
 PAYLOAD_BYTES = 2048
 PACKET_INTERVAL_MS = 1000
-ENVIRONMENT = "OPEN"
+ENVIRONMENT = "INDOOR"
 LINE_OF_SIGHT = False
-OBSTACLE_TYPE = "WALL"
+OBSTACLE_TYPE = "PEOPLE"
 TRAFFIC_REQUIREMENT = "TELEMETRY"
 TEST_DURATION_SECONDS = 20
 REPETITIONS = 3
@@ -71,7 +71,7 @@ BAUD_RATE = 115200
 # ============================================================
 #                    API CONFIGURATION
 # ============================================================
-API_BASE_URL = ("http://100.72.37.28:8000")
+API_BASE_URL = ("http://100.120.114.19:8000")
 MEASUREMENT_ENDPOINT = (f"{API_BASE_URL}/measurements")
 HTTP_TIMEOUT_SECONDS = 5
 
